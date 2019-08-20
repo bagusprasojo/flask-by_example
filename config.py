6a1e462410cdc3b01b8915da3228923d34df48e7
bagusprasojo <bagusprasojo@gmail.com>
2019-08-20 23:38:11 +0700

    berhasil upgrade db

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    DEBUG = False
-    TESTING = False
-    CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
-	#SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+	DEBUG = False
+	TESTING = False
+	CSRF_ENABLED = True
+	SECRET_KEY = 'this-really-needs-to-be-changed'
+	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
